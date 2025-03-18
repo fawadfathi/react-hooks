787c3536661551d4df666710d5b9b625297444d1
Ahmad Fawad Fathi <153498498+fawadfathi@users.noreply.github.com>
2025-03-18 15:13:44 +0430

    remove forward ref

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,29 +1,105 @@
-// useState: greeting
-// http://localhost:3000/isolated/exercise/01.js
+// TODO: Seriously, this is a mess specially with tailwind animation classes change it to smooth beautiful motion animation with framer motion.
+// TODO: Also, replace the @radix-ui/react-dialog with our own dialog component we don't need to use the @radix-ui/react-dialog.
 
 import * as React from 'react'
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+import {X} from 'lucide-react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+import {cn} from '../../lib/utils'
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
+const Dialog = DialogPrimitive.Root
 
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+const DialogTrigger = DialogPrimitive.Trigger
+
+const DialogPortal = DialogPrimitive.Portal
+
+const DialogClose = DialogPrimitive.Close
+
+const DialogOverlay = ({className, ...props}, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className={cn(
+      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-gray-800/50',
+      className,
+    )}
+    {...props}
+  />
+)
+
+const DialogContent = ({className, children, ...props}, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 bg-gray-800 p-6 shadow-lg duration-200 sm:rounded-lg',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      <DialogPrimitive.Close className="absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:bg-gray-700/10 focus:outline-none disabled:pointer-events-none data-[state=open]:text-gray-300 [&>svg]:text-gray-500 hover:[&>svg]:text-gray-300">
+        <X className="h-4 w-4" />
+        <span className="sr-only">Close</span>
+      </DialogPrimitive.Close>
+    </DialogPrimitive.Content>
+  </DialogPortal>
+)
+
+const DialogHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col space-y-1.5 text-center sm:text-left',
+      className,
+    )}
+    {...props}
+  />
+)
+
+const DialogFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className,
+    )}
+    {...props}
+  />
+)
+
+const DialogTitle = ({className, ...props}, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn(
+      'text-lg leading-none font-semibold tracking-tight text-gray-300',
+      className,
+    )}
+    {...props}
+  />
+)
+
+const DialogDescription = ({className, ...props}, ref) => (
+  <DialogPrimitive.Description
+    ref={ref}
+    className={cn('text-sm text-gray-500', className)}
+    {...props}
+  />
+)
+
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
 }
-
-function App() {
-  return <Greeting />
-}
-
-export default App
